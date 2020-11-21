806700b5889884ff413d699e0d6549dd7111fe87
Satou <satoutatsuhiro1@gmail.com>
2020-11-21 19:20:02 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var firstName = 'leftPart';
+var lastName = 'rightPart';
+var leftPart = undefined;
+var rightPart = undefined;
+
+var result = leftPart + rightPart;
+var result = 25 * 100;
+
+console.log(result);
